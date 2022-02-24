9bb8108c5f037e224a348ad63711a42429daa6fd
MontyPan <monty.pan@dontcareabout.us>
2022-02-25 00:24:52 +0800

    refactor：用 Member 重新設計 field()→member()

--- src/main/java/us/dontcareabout/starpocks/util/ClassUtil.java
+++ src/main/java/us/dontcareabout/starpocks/util/ClassUtil.java
@@ -5,6 +5,7 @@ import static java.lang.reflect.Modifier.isProtected;
 import static java.lang.reflect.Modifier.isPublic;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Member;
 import java.lang.reflect.Modifier;
 import java.util.Comparator;
 import java.util.List;
@@ -13,31 +14,40 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ClassUtil {
+	private static final Predicate<Member> publicFilter = f -> Modifier.isPublic(f.getModifiers());
+	private static final Predicate<Member> protectedFilter = f -> Modifier.isProtected(f.getModifiers());
+	private static final Predicate<Member> packageFilter = f -> isPackage(f.getModifiers());
+	private static final Predicate<Member> privateFilter = f -> Modifier.isPrivate(f.getModifiers());
+
 	public static boolean isPackage(int modifier) {
 		return !isPublic(modifier) && !isProtected(modifier) && !isPrivate(modifier);
 	}
 
+	////////////////
+
 	public static List<Field> publicField(Class<?> clazz, boolean isStatic) {
-		return field(clazz, isStatic, f -> Modifier.isPublic(f.getModifiers()));
+		return member(clazz.getDeclaredFields(), isStatic, publicFilter);
 	}
 
 	public static List<Field> protectedField(Class<?> clazz, boolean isStatic) {
-		return field(clazz, isStatic, f -> Modifier.isProtected(f.getModifiers()));
+		return member(clazz.getDeclaredFields(), isStatic, protectedFilter);
 	}
 
 	public static List<Field> packageField(Class<?> clazz, boolean isStatic) {
-		return field(clazz, isStatic, f -> isPackage(f.getModifiers()));
+		return member(clazz.getDeclaredFields(), isStatic, packageFilter);
 	}
 
 	public static List<Field> privateField(Class<?> clazz, boolean isStatic) {
-		return field(clazz, isStatic, f -> Modifier.isPrivate(f.getModifiers()));
+		return member(clazz.getDeclaredFields(), isStatic, privateFilter);
 	}
 
-	private static List<Field> field(Class<?> clazz, boolean isStatic, Predicate<Field> p) {
-		return Stream.of(clazz.getDeclaredFields())
-			.filter(p)
-			.filter(f -> isStatic == Modifier.isStatic(f.getModifiers()))
-			.sorted(Comparator.comparing(Field::getName))
+	////////////////
+
+	private static <M extends Member> List<M> member(M[] members, boolean isStatic, Predicate<Member> visibilityFilter) {
+		return Stream.of(members)
+			.filter(visibilityFilter)
+			.filter(m -> isStatic == Modifier.isStatic(m.getModifiers()))
+			.sorted(Comparator.comparing(Member::getName))
 			.collect(Collectors.toList());
 	}
 }
