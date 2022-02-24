0bfd8d057e9edce8ee9de2901272f7c58138beba
MontyPan <monty.pan@dontcareabout.us>
2022-02-24 22:37:46 +0800

    增加：只有 field 的 FullType

---
+++ src/main/java/us/dontcareabout/starpocks/sample/FullType.java
@@ -0,0 +1,21 @@
+package us.dontcareabout.starpocks.sample;
+
+@SuppressWarnings("unused")
+public class FullType {
+	public byte public_byte;
+	public Byte public_Byte;
+
+	protected short protected_short;
+	protected Short protected_Short;
+
+	int package_int;
+	Integer package_Integer;
+
+	private long private_long;
+	private Long private_Long;
+
+	public static Object staticPublic;
+	protected static Object staticProtected;
+	static Object staticPackage;
+	private static Object staticPrivate;
+}
