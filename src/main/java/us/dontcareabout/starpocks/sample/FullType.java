37a2d43afcea8ece81e764175cd30abdfe538aa7
MontyPan <monty.pan@dontcareabout.us>
2022-02-26 16:09:44 +0800

    增加：method

--- src/main/java/us/dontcareabout/starpocks/sample/FullType.java
+++ src/main/java/us/dontcareabout/starpocks/sample/FullType.java
@@ -25,4 +25,21 @@ public class FullType {
 	protected FullType(short value) {}
 	FullType(int value) {}
 	private FullType(long value) {}
+
+	public void publicMethod(String a, String b) {}
+	public void publicMethod(String... a) {}
+	public void publicMethod() {}
+	public void publicMethod(int a, int b, int c) {}
+	//用「a」開頭，以確定 constructor 會在最前面
+	public String aMethod(int index) { return null; }
+	public String aMethod(Integer index, int[] array) { return ""; }
+	protected void protectedMethod() {}
+	void packageMethod() {}
+	private void privateMethod() {}
+
+
+	public static String staticMethod(int value) { return ""; }
+	protected static String staticMethod(long value) { return ""; }
+	static String staticMethod(float value) { return ""; }
+	private static String staticMethod(double value) { return ""; }
 }
