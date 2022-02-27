013a0126836b12b3181d54180bae2883d3dfe51c
MontyPan <monty.pan@dontcareabout.us>
2022-02-28 02:24:27 +0800

    增加：ClassDiagram

---
+++ src/main/java/us/dontcareabout/starpocks/mermaid/ClassDiagram.java
@@ -0,0 +1,22 @@
+package us.dontcareabout.starpocks.mermaid;
+
+import java.util.List;
+
+import us.dontcareabout.starpocks.RelationAnalysis;
+import us.dontcareabout.starpocks.writer.ClassWriter;
+import us.dontcareabout.starpocks.writer.RelationWriter;
+
+public class ClassDiagram {
+	private ClassWriter classWriter = new ClassWriter();
+	private RelationWriter relationWriter = new RelationWriter();
+
+	public String write(List<Class<?>> clazzList) {
+		RelationAnalysis ra = new RelationAnalysis(clazzList);
+
+		StringBuilder result = new StringBuilder("classDiagram\n");
+		result.append(classWriter.write(ra.getAllClass()));
+		result.append("\n");
+		result.append(relationWriter.write(ra.getRelation()));
+		return result.toString();
+	}
+}
